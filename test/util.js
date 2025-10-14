852f35a13ddbfaa67561193e6b95d0d5d081c103
Victor Adossi <vadossi@cosmonic.com>
2025-10-14 23:13:39 +0900

    fix(ci): tests on windows runner

--- test/util.js
+++ test/util.js
@@ -24,6 +24,7 @@ export function maybeLogging(disableFeatures) {
   return disableFeatures;
 }
 
+// THE FUNCTION HANGS ON WINDOWS
 export async function setupComponent(opts) {
   const componentizeSrc = opts?.componentize?.src;
   const componentizeOpts = opts?.componentize?.opts;
